06a9769f311eb30e19bded9796f7a4a1fc100312
Matt Drees <matt@drees.me>
2020-01-06 16:41:56 -0800

    Creating and Combining Views" SwiftUI Essentials tutorial.
    Landmarks project from the tutorial.

---
+++ swift/Landmarks/Landmarks/Preview Content/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Matthew Drees on 1/6/20.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+        .clipShape(Circle())
+        .overlay(
+            Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
