59ed76ba482feecf18e625da02491106dd1bcaf9
Matt Drees <matt@drees.me>
2020-01-08 11:39:42 -0800

    Further walk down the swift tour, covering objects, classes, and enums.

--- swift/a_swift5.1_tour.swift
+++ swift/a_swift5.1_tour.swift
@@ -174,15 +174,199 @@ func functions_and_closures()
     let sortedNumbers = numbers.sorted { $0 > $1 }
     print(sortedNumbers)
 }
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+class Square: NamedShape {
+    var sideLength: Double
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
 
 func objects_and_classes()
 {
+    let shape = Shape()
+    shape.numberOfSides = 7
+    print(shape.simpleDescription())
+    shape.numberOfSides = 8 // "let" is a lie! Or at least it doesn't mean "const".
+    print(shape.simpleDescription())
+
+    let test = Square(sideLength: 5.2, name: "my test square")
+    print(test.area())
+    print(test.simpleDescription())
+
+    let triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+    print(triangle.perimeter)
+    triangle.perimeter = 9.9
+    print(triangle.perimeter)
+
+    let triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+    print(triangleAndSquare.square.sideLength)
+    print(triangleAndSquare.triangle.sideLength)
+    triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+    print(triangleAndSquare.triangle.sideLength)
+
+    let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+    let sideLength = optionalSquare?.sideLength
+    print(sideLength!)
 }
+
+func enumerations_and_structures()
+{
+    enum Rank: Int {
+        case ace = 1
+        case two, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king
+
+        func simpleDescription() -> String {
+            switch self {
+            case.ace:
+                return "ace"
+            case.jack:
+                return "jack"
+            case .queen:
+                return "queen"
+            case .king:
+                return "king"
+            default:
+                return String(self.rawValue)
+            }
+        }
+    }
+    let ace = Rank.ace
+    print(ace)
+    let aceRawValue = ace.rawValue
+    print(aceRawValue)
+
+    if let convertedRank = Rank(rawValue: 11) {
+        let threeDescription = convertedRank.simpleDescription()
+        print(threeDescription)
+    }
+
+    enum Suit {
+        case spades, hearts, diamonds, clubs
+
+        func simpleDescription() -> String {
+            switch self {
+            case .spades:
+                return "spades"
+            case .hearts:
+                return "hearts"
+            case .diamonds:
+                return "diamonds"
+            case .clubs:
+                return "clubs"
+            }
+        }
+    }
+    let hearts = Suit.hearts
+    print(hearts)
+    let heartsDescription = hearts.simpleDescription()
+    print(heartsDescription)
+
+    enum ServerResponse {
+        case result(String, String)
+        case failure(String)
+    }
+
+    let success = ServerResponse.result("6:00 am", "8:09 pm")
+    let failure = ServerResponse.failure("Out of cheese.")
+    func printMessage(response: ServerResponse) {
+        switch response {
+        case let .result(sunrise, sunset):
+            print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
+        case let .failure(message):
+            print("Failure...  \(message)")
+        }
+    }
+    printMessage(response: success)
+    printMessage(response: failure)
+
+    struct Card {
+        var rank: Rank
+        var suit: Suit
+        func simpleDescription() -> String {
+            return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+        }
+    }
+    let threeOfSpades = Card(rank: .three, suit: .spades)
+    print(threeOfSpades.simpleDescription())
+}
+
 simplevalues()
 controlflow()
 functions_and_closures()
 objects_and_classes()
-// enumerations_and_structures()
+enumerations_and_structures()
 // protocols_and_extensions()
 // error_handling()
 // generics()
